c844f36f450a21c1a0ce308403116ce62073510e
Ian Bonafede <ibona001@ucr.edu>
2019-02-01 11:33:37 -0800

    blah

--- main.cpp
+++ main.cpp
@@ -2,12 +2,23 @@
 #include "cpp.h"
 #include <string.h>
 using namespace std;
-extern "C" void sim() // main process
+void Bob(); // Another process
+extern "C" void sim() // Alice is the main process
 {
 int i;
-create("sim"); // separate from background system
+create("Alice"); // Alice separates from background system
+Bob(); // Alice gives Bob a chance to do the same
 for(i=1;i<10;i++)
 {
 cout << "Alice says hello " << i << " times." << endl;
+}
+}
+void Bob()
+{
+int i;
+create("Bob");
+for(i=1;i<10;i++)
+{
+cout << "Bob says hello " << i << " times." << endl;
 }
 }
