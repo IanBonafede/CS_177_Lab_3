bc0e7c263febef3fbde19af4c4a786070f4f0374
Ian Bonafede <ibona001@ucr.edu>
2019-01-31 20:06:18 -0800

    makefile3

--- main.cpp
+++ main.cpp
@@ -1,31 +1,13 @@
-/*this CSIM program simulates an M/M/1 service center*/
-
-
-
-#include <cpp.h> /*include the CSIM C++ header file*/
-facility *f; /*the service center*/
-
-
-
-
-
-
-extern “C” void sim() /*sim process*/
+#include <iostream>
+#include "cpp.h"
+#include <string.h>
+using namespace std;
+extern "C" void sim() // main process
 {
- create("sim"); /*make this a process*/
- f = new facility("f"); /*create the service center - f*/
- while(simtime() < 5000.0) { /*loop until end of simulation*/
- hold(exponential(1.0)); /*delay between customer arrivals*/
- customer(); /*generate new customer*/
- }
- report(); /*produce statistics report*/
+int i;
+create("sim"); // separate from background system
+for(i=1;i<10;i++)
+{
+cout << "Alice says hello " << i << " times." << endl;
 }
-
-
-
-
-void customer()
-{
- create("customer"); /*make this a process*/
- f->use(exponential(0.5)); /*obtain needed amount of service*/
 }
